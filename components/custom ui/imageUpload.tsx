50270c7dc828e6389bf85fba9e9170372b89bcd3
newsarawut <sarawutch572@gmail.com>
2024-05-14 11:18:28 +0700

    ImageUpload

--- components/custom ui/imageUpload.tsx
+++ components/custom ui/imageUpload.tsx
@@ -2,10 +2,12 @@ import { CldUploadWidget } from "next-cloudinary";
 import { Plus, Trash } from "lucide-react";
 
 import { Button } from "../ui/button";
+import React from "react";
 import Image from "next/image";
 
 interface ImageUploadProps {
   value: string[];
+
   onChange: (value: string) => void;
   onRemove: (value: string) => void;
 }
@@ -26,7 +28,6 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           <div key={url} className="relative w-[200px] h-[200px]">
             <div className="absolute top-0 right-0 z-10">
               <Button
-                type="button"
                 onClick={() => onRemove(url)}
                 size="sm"
                 className="bg-red-1 text-white"
@@ -36,15 +37,14 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
             </div>
             <Image
               src={url}
-              alt="collection"
+              alt="Collection"
               className="object-cover rounded-lg"
               fill
             />
           </div>
         ))}
       </div>
-
-      <CldUploadWidget uploadPreset="dolihepqq" onUpload={onUpload}>
+      <CldUploadWidget uploadPreset="gk7dqnog" onUpload={onUpload}>
         {({ open }) => {
           return (
             <Button
@@ -52,8 +52,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               onClick={() => open()}
               className="bg-grey-1 text-white"
             >
-              <Plus className="h-4 w-4 mr-2" />
-              Upload Image
+              <Plus className="w-4 h-4 mr-2" /> Upload Image
             </Button>
           );
         }}
