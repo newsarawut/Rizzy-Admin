4e0f96cb23059aad0766d4c3390578ab593f3334
newsarawut <sarawutch572@gmail.com>
2024-05-14 10:59:19 +0700

    OrderId

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -1,67 +1,42 @@
-"use client";
-
 import { DataTable } from "@/components/custom ui/DataTable";
 import { columns } from "@/components/orderItems/OrderItemsColumns";
-import { useEffect, useState } from "react";
 
-const OrderDetails = ({ params }: { params: { orderId: string } }) => {
-  const [orderId, setOrderId] = useState("");
-  const [customerName, setCustomerName] = useState("");
-  const [street, setStreet] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
-  const [totalAmount, setTotalAmout] = useState("");
-  const [shippingRate, setShippingRate] = useState("");
-  const [products, setProducts] = useState([]);
+const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
+  const res = await fetch(
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`
+  );
+  const { orderDetails, customer } = await res.json();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(
-          `${process.env.ADMIN_DASHBOARD_URL}/orders/${params.orderId}`
-        );
-        const { orderDetails, customer } = await res.json();
-        setOrderId(orderDetails._id);
-        setCustomerName(customer.name);
-        setStreet(orderDetails.shippingAddress.street);
-        setCity(orderDetails.shippingAddress.city);
-        setState(orderDetails.shippingAddress.state);
-        setPostalCode(orderDetails.shippingAddress.postalCode);
-        setCountry(orderDetails.shippingAddress.country);
-        setTotalAmout(orderDetails.totalAmount);
-        setShippingRate(orderDetails.shippingRate);
-        setProducts(orderDetails.products);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, [params.orderId]);
+  const { street, city, state, postalCode, country } =
+    orderDetails.shippingAddress;
 
   return (
     <div className="flex flex-col p-10 gap-5">
       <p className="text-base-bold">
-        Order ID: <span className="text-base-medium">{orderId}</span>
+        Order ID: <span className="text-base-medium">{orderDetails._id}</span>
       </p>
       <p className="text-base-bold">
-        Customer Name: <span className="text-base-medium">{customerName}</span>
+        Customer name: <span className="text-base-medium">{customer.name}</span>
       </p>
       <p className="text-base-bold">
-        Shipping Address:{" "}
+        Shipping address:{" "}
         <span className="text-base-medium">
           {street}, {city}, {state}, {postalCode}, {country}
         </span>
       </p>
       <p className="text-base-bold">
-        Total Paid: <span className="text-base-medium">$ {totalAmount}</span>
+        Total Paid:{" "}
+        <span className="text-base-medium">${orderDetails.totalAmount}</span>
       </p>
       <p className="text-base-bold">
         Shipping rate ID:{" "}
-        <span className="text-base-medium">{shippingRate}</span>
+        <span className="text-base-medium">{orderDetails.shippingRate}</span>
       </p>
-      <DataTable columns={columns} data={products} searchKey="product" />
+      <DataTable
+        columns={columns}
+        data={orderDetails.products}
+        searchKey="product"
+      />
     </div>
   );
 };
