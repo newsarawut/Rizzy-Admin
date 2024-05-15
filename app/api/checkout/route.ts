0e2e0124fd6b6d08518ca1a6da1ef78e6487e941
newsarawut <sarawutch572@gmail.com>
2024-05-15 16:58:53 +0700

    checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 
 const corsHeaders = {
+  'Access-Control-Allow-Credentials': "true",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
 };
 
 export async function POST(req: NextRequest) {
